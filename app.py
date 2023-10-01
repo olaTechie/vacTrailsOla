ac008294d8efc9ae5cd57edcae112f12281e0c5f
Ola Uthman <olatechie.ai@gmail.com>
2023-10-01 13:22:39 +0100

    button added after screen2

--- app.py
+++ app.py
@@ -41,7 +41,72 @@ def app():
     study = cur.execute('SELECT * FROM studies WHERE Decision IS NULL LIMIT 1').fetchone()
 
     if study:
-        # (The rest of the code remains the same)
+        # Calculate the total number of studies, number screened, and number remaining
+        total_studies = cur.execute('SELECT COUNT(*) FROM studies').fetchone()[0]
+        screened_studies = cur.execute('SELECT COUNT(*) FROM studies WHERE Decision IS NOT NULL').fetchone()[0]
+        remaining_studies = total_studies - screened_studies
+
+        # Display the row number and study statistics in the same row as cards
+        st.markdown(f"### Study {study[0] + 1}")  
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.markdown(f"**Total Studies**\n\n{total_studies}")
+        with col2:
+            st.markdown(f"**Screened Studies**\n\n{screened_studies}")
+        with col3:
+            st.markdown(f"**Remaining Studies**\n\n{remaining_studies}")
+
+        # Display the specified eight columns with their respective column numbers
+        st.write(f"NCT Number: {study[1]}")  
+        st.write(f"Study Title: {study[2]}")
+        st.write(f"Brief Summary: {study[3]}")  
+        st.write(f"Conditions: {study[4]}")  
+        st.write(f"Interventions: {study[5]}")  
+        st.write(f"Locations: {study[6]}")  
+        st.write(f"Countries: {study[7]}")
+        st.write(f"MCountries: {study[8]}")  
+
+        # Buttons to include, exclude, or move to the next study
+        col1, col2, col3 = st.columns(3)
+        if col1.button('Include'):
+            cur.execute('UPDATE studies SET Decision = ? WHERE id = ?', ('Include', study[0]))
+            conn.commit()
+        if col2.button('Exclude'):
+            cur.execute('UPDATE studies SET Decision = ? WHERE id = ?', ('Exclude', study[0]))
+            conn.commit()
+        if col3.button('Next'):
+            # Just refresh the page to get the next study with missing Decision
+            st.experimental_rerun()
+
+        # Display decision summary
+        included_count = cur.execute('SELECT COUNT(*) FROM studies WHERE Decision = "Include"').fetchone()[0]
+        excluded_count = cur.execute('SELECT COUNT(*) FROM studies WHERE Decision = "Exclude"').fetchone()[0]
+        undecided_count = cur.execute('SELECT COUNT(*) FROM studies WHERE Decision IS NULL').fetchone()[0]
+
+        # Plot pie chart and bar chart side by side
+        col1, col2 = st.columns(2)
+        with col1:
+            labels = ['Included', 'Excluded', 'Undecided']
+            sizes = [included_count, excluded_count, undecided_count]
+            colors = ['green', 'red', 'grey']
+            explode = (0.1, 0.1, 0.1)  
+
+            plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+            plt.axis('equal')
+            st.pyplot()
+
+        # Plot bar chart for study statistics
+        with col2:
+            labels = ['Total Studies', 'Screened Studies', 'Remaining Studies']
+            values = [total_studies, screened_studies, remaining_studies]
+            index = np.arange(len(labels))
+
+            plt.bar(index, values, color=['blue', 'orange', 'green'])
+            plt.xlabel('Categories', fontsize=12)
+            plt.ylabel('Number of Studies', fontsize=12)
+            plt.xticks(index, labels, fontsize=12, rotation=30)
+            plt.title('Study Statistics')
+            st.pyplot()
 
     # If all studies have been screened
     else:
